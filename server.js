e7c9f0d89f39a944b04741010804decdb6d1c203
LarahImports <Athomazg@gmail.com>
2026-05-17 17:26:44 -0400

    Increase JSON body limit to 50mb

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const transporter = nodemailer.createTransport({
 });
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.static(__dirname));
   
 const pool = new Pool({
